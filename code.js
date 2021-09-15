acdd3d8b9e2654405c9011e91ddade130033e9c3
Tristan Alie <TristanAlie@yahoo.com>
2021-09-15 13:49:30 -0400

    added comments

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 // Your code here
-const vowelChecker = (letter) => {
-    if(letter.toLowerCase() === 'a' || letter.toLowerCase() === 'e' || letter.toLowerCase() === 'i' || letter.toLowerCase() === 'o' || letter.toLowerCase() === 'u') {
+const vowelChecker = (letter) => { //check if a letter is a vowel, returns a boolean
+    if(letter.toLowerCase() === 'a' || letter.toLowerCase() === 'e' || letter.toLowerCase() === 'i' || letter.toLowerCase() === 'o' || letter.toLowerCase() === 'u') { //the logic for "y" is more complicated so i haven't implemented it yet
         console.log(letter)
         return true
     } else {
@@ -8,27 +8,27 @@ const vowelChecker = (letter) => {
     }
 }
 
-const convertToPigLatin = (str) => {
-    if(typeof str !== 'string') {
+const convertToPigLatin = (str) => { //the function to convert pig latin
+    if(typeof str !== 'string') { //checks if it's a string
         return false
     } else {
-        let wordArr = str.split(' ')
+        let wordArr = str.split(' ') //splits the string into words
 
-        successArr = []
-        for(let word of wordArr) {
-            let letters = word.split('')
-            let pigLatinArr = []
-            let firstVowelIndex = letters.findIndex(letter => vowelChecker(letter))
-            for(let i = firstVowelIndex; i < letters.length; i++) {
+        successArr = [] //a place to store the converted words
+        for(let word of wordArr) { //runs for each word individually
+            let letters = word.split('') //converts the word into letters
+            let pigLatinArr = [] //a place to store the converted word
+            let firstVowelIndex = letters.findIndex(letter => vowelChecker(letter)) //finds the first vowel and saves the index
+            for(let i = firstVowelIndex; i < letters.length; i++) { //a loop that starts at the first vowel
                 pigLatinArr.push(letters[i])
             }
-            pigLatinArr.push('-')
-            for(let i = 0; i < firstVowelIndex; i++) {
+            pigLatinArr.push('-') //puts a dash between the word and the ay
+            for(let i = 0; i < firstVowelIndex; i++) { //puts the consonants after the dash
                 pigLatinArr.push(letters[i])
             }
-            pigLatinArr.push('ay')
-            successArr.push(pigLatinArr.join(''))
+            pigLatinArr.push('ay') // adds the ay to the end
+            successArr.push(pigLatinArr.join('')) //turns the converted word back into a word and adds it to the converted words array
         }
-        return successArr.join(' ')
+        return successArr.join(' ') //turns the array into a string and returns the converted string
     }
 }
